4e0287a9b9eca3aa6e20d7122c4681893e84bf2a
nikolay5r <nradilov30@gmail.com>
2024-11-13 15:17:00 +0200

    Spawned stars
    Drew 20 random stars on surface

--- SpaceShooter/code/main.py
+++ SpaceShooter/code/main.py
@@ -1,6 +1,7 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 from os.path import join
+import random
 
 IMAGES_PATH = join("assets", "images")
 AUDIO_PATH = join("assets", "audio")
@@ -13,16 +14,20 @@ pygame.display.set_caption("Space Shooter")
 running = True
 
 player_surface = pygame.image.load(join(IMAGES_PATH, "player.png")).convert_alpha()
+star_surface = pygame.image.load(join(IMAGES_PATH, "star.png")).convert_alpha()
+star_positions = [(random.randint(0 + star_surface.size[0], WINDOW_WIDTH - star_surface.size[0]),
+             random.randint(0 + star_surface.size[1], WINDOW_HEIGHT - star_surface.size[1])) for i in range(20)]
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-            
     display_surface.fill("darkgray")
     display_surface.blit(player_surface, (100, 200)) 
-    
-    pygame.display.update() 
+    for pos in star_positions:
+        display_surface.blit(star_surface, pos)
+
+    pygame.display.update()
 
 pygame.quit()
