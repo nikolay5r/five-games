9264babdd89160663b98d7a62b9e2e8aff3f6af8
nikolay5r <nradilov30@gmail.com>
2024-11-17 00:33:47 +0200

    Created Game class for game logic

--- VampireSurvivor/code/main.py
+++ VampireSurvivor/code/main.py
@@ -1,2 +1,32 @@
 from settings import *
+from sprites import *
+from player import Player
+import surfaces
 
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Vampire Survivor")
+        self.all_sprites = pygame.sprite.Group()
+        self.clock = pygame.time.Clock()
+        self.running = True
+    
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.display_surface.fill("lightgreen")
+            self.all_sprites.update(dt)            
+            self.all_sprites.draw(self.display_surface)
+
+            pygame.display.update()
+        
+        pygame.quit()
+
+game = Game()
+game.run()
--- VampireSurvivor/code/settings.py
+++ VampireSurvivor/code/settings.py
@@ -1,6 +1,11 @@
 import pygame 
 from os.path import join 
 from os import walk
+import random
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720 
 TILE_SIZE = 64
+
+PLAYER_PATH = join("images", "player")
+ENEMIES_PATH = join("images", "enemies")
+GUN_PATH = join("images", "gun")
