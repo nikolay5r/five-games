763d31c1edeb95bb564ace5b369f0305b655b336
nikolay5r <nradilov30@gmail.com>
2024-11-20 22:54:46 +0200

    Added enemy frames

--- VampireSurvivor/code/surfaces.py
+++ VampireSurvivor/code/surfaces.py
@@ -7,5 +7,11 @@ PLAYER_SURFACE_FRAMES = {
     "right": tuple(pygame.image.load(join(PLAYER_PATH, "right", f"{i}.png")) for i in range(4))
 }
 
+ENEMY_SURFACES_FRAMES = (
+    tuple(pygame.image.load(join(ENEMIES_PATH, "bat", f"{i}.png")) for i in range(4)),
+    tuple(pygame.image.load(join(ENEMIES_PATH, "blob", f"{i}.png")) for i in range(4)),
+    tuple(pygame.image.load(join(ENEMIES_PATH, "skeleton", f"{i}.png")) for i in range(4))
+)
+
 BULLET_SURFACE = pygame.image.load(join(GUN_PATH, "bullet.png"))
 GUN_SURFACE = pygame.image.load(join(GUN_PATH, "gun.png"))
