76142e365d042ce49e2d276713d16531ffc6fb5c
nikolay5r <nradilov30@gmail.com>
2024-12-28 00:58:36 +0200

    Add simple movement mechanics

--- Platformer/code/sprites.py
+++ Platformer/code/sprites.py
@@ -11,8 +11,28 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.setup_frames()
         self.image = self.frames[0]
-        self.rect = self.image.get_frect(center = pos)
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+        #movement
+        self.direction = pygame.math.Vector2()
+        self.speed = 200
+
+    def move(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+
+    def get_direction(self):
+        keys = pygame.key.get_pressed()
+        
+        self.direction.x = int(keys[pygame.K_d] or keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
+        
+        if self.direction:
+            self.direction = self.direction.normalize()
 
     def setup_frames(self):
         for root, _, files in walk(join("images", "player")):
             self.frames = [pygame.image.load(join(root, file)) for file in files]
+
+    def update(self, dt):
+        self.get_direction()
+        self.move(dt)
