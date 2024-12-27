37980bc7e1bc6d375999b86cedf0ea521f42e5b0
nikolay5r <nradilov30@gmail.com>
2024-12-28 00:41:08 +0200

    Add player class

--- Platformer/code/sprites.py
+++ Platformer/code/sprites.py
@@ -6,3 +6,13 @@ class Sprite(pygame.sprite.Sprite):
         self.image = surf 
         self.rect = self.image.get_frect(center = pos)
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.setup_frames()
+        self.image = self.frames[0]
+        self.rect = self.image.get_frect(center = pos)
+
+    def setup_frames(self):
+        for root, _, files in walk(join("images", "player")):
+            self.frames = [pygame.image.load(join(root, file)) for file in files]
