abd8584e5991514160193d9a5d2d84ad94fe0063
nikolay5r <nradilov30@gmail.com>
2024-12-29 16:00:41 +0200

    Add simple Timer

--- Platformer/code/timers.py
+++ Platformer/code/timers.py
@@ -28,8 +28,7 @@ class FunctionTimer:
     def update(self):
         curr_time = pygame.time.get_ticks()
         if self.active and curr_time - self.start_time >= self.duration:
-            if self.func:
-                self.func(*self.params)
+            self.func(*self.params)
             self.deactivate()
 
 class EventTimer:
@@ -59,6 +58,33 @@ class EventTimer:
     def update(self):
         curr_time = pygame.time.get_ticks()
         if self.active and curr_time - self.start_time >= self.duration:
-            if self.event_type:
-                pygame.event.post(pygame.Event(self.event_type))
+            pygame.event.post(pygame.Event(self.event_type))
+            self.deactivate()
+    
+class Timer:
+    def __init__(self, duration, repeat = False, auto_start = False):
+        self.duration = duration
+        self.repeat = repeat
+        self.active = False
+        self.start_time = 0
+
+        if auto_start:
+            self.activate()
+
+    def __bool__(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+        self.start_time = pygame.time.get_ticks()
+    
+    def deactivate(self):
+        self.active = False
+        self.start_time = 0
+        if self.repeat:
+            self.activate()
+    
+    def update(self):
+        curr_time = pygame.time.get_ticks()
+        if self.active and curr_time - self.start_time >= self.duration:
             self.deactivate()
