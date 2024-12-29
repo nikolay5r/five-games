195d22bc8bc71ce14eb508a63ab89e7ddd1a7dbd
nikolay5r <nradilov30@gmail.com>
2024-12-29 14:40:26 +0200

    Add new Function Timer class
    The old Timer class is now EventTimer and the new timer class is FunctionTimer. The difference is that one class is for events and the other for handling functions

--- Platformer/code/timers.py
+++ Platformer/code/timers.py
@@ -1,7 +1,39 @@
 from settings import *
 
-class Timer:
-    def __init__(self, duration, event_type: int = None, repeat = False, auto_start = False):
+class FunctionTimer:
+    def __init__(self, duration, func, *params, repeat = False, auto_start = False):
+        self.duration = duration
+        self.func = func
+        self.params = params
+        self.repeat = repeat
+        self.active = False
+        self.start_time = 0
+
+        if auto_start:
+            self.activate()
+
+    def __bool__(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+        self.start_time = pygame.time.get_ticks()
+    
+    def deactivate(self):
+        self.active = False
+        self.start_time = 0
+        if self.repeat:
+            self.activate()
+    
+    def update(self):
+        curr_time = pygame.time.get_ticks()
+        if self.active and curr_time - self.start_time >= self.duration:
+            if self.func:
+                self.func(*self.params)
+            self.deactivate()
+
+class EventTimer:
+    def __init__(self, duration, event_type: int, repeat = False, auto_start = False):
         self.duration = duration
         self.event_type = event_type
         self.repeat = repeat
